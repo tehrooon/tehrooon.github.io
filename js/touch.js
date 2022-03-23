34c4b06ea249fb99d7442141fc39b5c0f9d88e75
amir <amirghassempour@protonmail.com>
2022-03-24 03:13:27 +0430

    some changes on js/touch; commit to test

--- js/touch.js
+++ js/touch.js
@@ -1,9 +1,12 @@
-//var article = document.getElementsByTagName("article")[0];
-//var style = "width: 20%; height: 20%; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
-//var qrCode = '<img src="../media/svg/qr-code.svg" /><img style="' + style + '" src="../media/amir.webp" />';
-//article.innerHTML = qrCode // trigger.
+function showQrCode() {
+	var article = document.getElementsByTagName("article")[0];
+	var style = "width: 20%; height: 20%; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+	var qrCode = '<img src="../media/svg/qr-code.svg" /><img style="' + style + '" src="../media/amir.webp" />';
+	article.innerHTML = qrCode // trigger.
+}
 var article = document.getElementsByTagName("article")[0];
 article.addEventListener("touchstart", function(event) {
-  window.alert(event.touches.length)
-  window.alert("hello")
+	if (event.touches.length == 3) {
+		showQrCode()
+	}
 }, false)
