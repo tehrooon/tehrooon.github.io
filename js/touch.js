40162f9cf19278436483fd7b13fb72f4fcc9a561
amir <amirghassempour@protonmail.com>
2022-03-24 03:02:31 +0430

    change js/touch to test touch

--- js/touch.js
+++ js/touch.js
@@ -1,5 +1,9 @@
+//var article = document.getElementsByTagName("article")[0];
+//var style = "width: 20%; height: 20%; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+//var qrCode = '<img src="../media/svg/qr-code.svg" /><img style="' + style + '" src="../media/amir.webp" />';
+//article.innerHTML = qrCode // trigger.
 var article = document.getElementsByTagName("article")[0];
-var style = "width: 20%; height: 20%; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
-var qrCode = '<img src="../media/svg/qr-code.svg" /><img style="' + style + '" src="../media/amir.webp" />';
-article.innerHTML = qrCode // trigger.
-
+article.addEventListener("touchstart", function(event) {
+  window.alert(event.touches)
+  window.alert("hello")
+}, false)
