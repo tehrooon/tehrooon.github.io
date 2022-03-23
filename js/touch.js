b7735fb6f1c05b35aac712e792bd0d53edd43b14
amir <amirghassempour@protonmail.com>
2022-03-24 04:18:02 +0430

    handle qrcode show and hide

--- js/touch.js
+++ js/touch.js
@@ -1,12 +1,20 @@
+var article = document.getElementsByTagName("article")[0];
+var bc = article.innerHTML;
 function showQrCode() {
-	var article = document.getElementsByTagName("article")[0];
 	var style = "width: 20%; height: 20%; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
 	var qrCode = '<img src="../media/svg/qr-code.svg" /><img style="' + style + '" src="../media/amir.webp" />';
-	article.innerHTML = qrCode // trigger.
+	article.innerHTML = qrCode; // trigger.
 }
-var article = document.getElementsByTagName("article")[0];
+
+function handleQrCode() {
+	if (article.innerHTML == bc) {
+		showQrCode();
+	} else {article.innerHTML = bc}
+}
+
+// var article = document.getElementsByTagName("article")[0];
 article.addEventListener("touchstart", function(event) {
 	if (event.touches.length == 3) {
-		showQrCode()
+		handleQrCode();
 	}
 }, false)
