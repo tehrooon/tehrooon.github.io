0a84c1e2218daf6545d540e3df8bf15f3001de18
amir <amirghassempour@protonmail.com>
2022-03-24 04:41:15 +0430

    final touches of touch.js; add ondblclick to contact/index#amir

--- js/touch.js
+++ js/touch.js
@@ -1,20 +1,26 @@
-var article = document.getElementsByTagName("article")[0];
-var bc = article.innerHTML;
+var article = document.getElementsByTagName("article")[0]; // define the article
+var bc = article.innerHTML; // define variable bc that includes the business card.
+
+// define a function to overwrites the content of article with qrCode var.
 function showQrCode() {
-	var style = "width: 20%; height: 20%; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
-	var qrCode = '<img src="../media/svg/qr-code.svg" /><img style="' + style + '" src="../media/amir.webp" />';
+	var style = "width: 10rem; height: 10rem; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+	var qrCode = '<img src="../media/svg/qr-code.svg" /><img id="amir" ondblclick="handleQrCode()" style="' + style + '" src="../media/amir.webp" />';
 	article.innerHTML = qrCode; // trigger.
 }
 
+// define a function to handle qr-code to bc and bc to qr-code
 function handleQrCode() {
 	if (article.innerHTML == bc) {
 		showQrCode();
 	} else {article.innerHTML = bc}
 }
 
-// var article = document.getElementsByTagName("article")[0];
+// add an event listener to listen for three start touches.
 article.addEventListener("touchstart", function(event) {
-	if (event.touches.length == 3) {
+	var countTouches = event.touches.length;
+	if (countTouches == 3) {
 		handleQrCode();
 	}
 }, false)
+
+// define 3 fingers touch and functions to show and hide article.innerHTML.
