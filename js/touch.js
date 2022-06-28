c144d0839ca280913f9aa6b49447608735812682
amir <amirghassempour@protonmail.com>
2022-06-28 19:08:52 +0430

    keyboard shortcut - test q and r together

--- js/touch.js
+++ js/touch.js
@@ -26,7 +26,7 @@ article.addEventListener("touchstart", function(event) {
 
 // add an event listener for keyboard shortcut.
 document.addEventListener("keydown", function(event) {
-	if (event.which == 71) {
+	if (event.which == 81 && event.which == 82) {
 		handleQrCode();
 	}
 })
