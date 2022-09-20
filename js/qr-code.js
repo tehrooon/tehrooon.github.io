f48f693ce53c4ba896bdf02e97ace7f4ebc1efae
amir <amirghassempour@protonmail.com>
2022-09-20 19:10:10 +0000

    update qr-code.js

--- js/qr-code.js
+++ js/qr-code.js
@@ -3,7 +3,7 @@ var bc = article.innerHTML; // define variable bc that includes the business car
 
 // function: overwrite article content with qrCode var.
 function showQrCode() {
-	var style = "width: 15%; height: 15%; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+	var style = "width: 20rem; height: 20rem; display: block; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
 	var qrCode = '<img src="../media/svg/qr-code.svg" /><img id="amir" ondblclick="handleQrCode()" style="' + style + '" src="../media/amir.webp" />';
 	article.innerHTML = qrCode; // trigger.
 }
