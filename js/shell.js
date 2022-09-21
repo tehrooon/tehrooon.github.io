7dfc07de7effe569aec66cfeb8f9189468b3be58
amir <amirghassempour@protonmail.com>
2022-09-21 22:51:31 +0000

    update new index

--- js/shell.js
+++ js/shell.js
@@ -1,11 +1,23 @@
 var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
+var ps1 = 'amir@github.io:~$ ';
+var txt = 'myname'; /* The text */
+var result = 'amir';
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
+  document.getElementById("ps1").innerHTML = ps1;
   if (i < txt.length) {
     document.getElementById("demo").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
+}
+
+function endit() {
+  document.getElementById("result").innerHTML = result;
+}
+
+function final() {
+	typeWriter();
+	endit();
 }
