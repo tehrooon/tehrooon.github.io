e32b6a15630f2751a74cd1058228c09333b6915a
amir <amirghassempour@protonmail.com>
2022-12-12 16:21:26 +0000

    update js/shell.js

--- js/shell.js
+++ js/shell.js
@@ -1,23 +1,33 @@
 var i = 0;
 var ps1 = 'amir@github.io:~$ ';
-var txt = 'myname'; /* The text */
-var result = 'amir';
+var input = commands[0][0]; /* input */
+var output = commands[0][1]; /* output */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   document.getElementById("ps1").innerHTML = ps1;
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+  if (i < input.length) {
+    document.getElementById("demo").innerHTML += input.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
 
 function endit() {
-  document.getElementById("result").innerHTML = result;
+  document.getElementById("result").innerHTML = output;
 }
 
+/*
 function final() {
 	typeWriter();
 	endit();
 }
+*/
+
+let myPromise = new Promise(function(myResolv, myReject) {
+	typeWriter();
+});
+
+myPromise.then(function(value) {
+	endit();
+});
