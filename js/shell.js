618bbb41d2ab2ecaaa65f0cdc2f0f45c13b5a716
amir <amirghassempour@protonmail.com>
2022-12-15 12:28:50 +0000

    shell.js: almost done with promises

--- js/shell.js
+++ js/shell.js
@@ -1,33 +1,42 @@
-var i = 0;
+const commands = [
+	["guest", "logged in as guest"],
+	["whoami", "amir"],
+	["exho mail", "4mi2@proton.me"],
+	["echo eth", "0x411112"]
+];
+
+var scr = document.getElementById("screen")
+var speed = 50;
+var login = 'login: ';
 var ps1 = 'amir@github.io:~$ ';
-var input = commands[0][0]; /* input */
-var output = commands[0][1]; /* output */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
-  document.getElementById("ps1").innerHTML = ps1;
-  if (i < input.length) {
-    document.getElementById("demo").innerHTML += input.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+
+async function user(t) {
+	let yourPromise = new Promise(function(yourResolve) {
+		let i = 0;
+		var input = commands[t][0];
+		var output = commands[t][1];
+		async function typeWriter() {
+			let myPromise = new Promise(function(myResolve) {
+				if (i < input.length) {
+					scr.innerHTML += input.charAt(i);
+					i++;
+					setTimeout(typeWriter, speed);
+				} else if (i == input.length) {
+					myResolve(output)
+					yourResolve(ps1);
+				};
+			});
+			scr.innerHTML += "<br />" + await myPromise;
+			scr.innerHTML += "<br />" + ps1;
+			//yourResolve(ps1);
+		};
+		typeWriter();
+		//typeWriter().then(function(value) {yourResolve(ps1);});
+	});
+	scr.innerHTML += "<br />" + await yourPromise;
 }
 
-function endit() {
-  document.getElementById("result").innerHTML = output;
-}
 
-/*
-function final() {
-	typeWriter();
-	endit();
-}
-*/
-
-let myPromise = new Promise(function(myResolv, myReject) {
-	typeWriter();
-});
-
-myPromise.then(function(value) {
-	endit();
-});
+scr.innerHTML += "<br />" + login;
+user(0).then(function(value) {user(1).then(function(value) {user(2);});});
