81c91e42ba359355c3906648958ed4e46e3a6df9
amir <amirghassempour@protonmail.com>
2022-12-16 21:04:25 +0000

    /index.html remove pre & remove login from commands

--- js/shell.js
+++ js/shell.js
@@ -1,5 +1,4 @@
 const commands = [
-	["guest", "logged in as guest"],
 	["whoami", "amir"],
 	["gpg -k", 'my pgp public key:<br />rsa4096 <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/C1826531F0F312291A461373479EC0B4C6FD024B">C1826531F0F312291A461373479EC0B4C5FD024B</a>'],
 	["echo $mailAddresses", "theamir@tutanota.com<br />4mi2@proton.me<br />please send mail from the same mail service."],
@@ -8,7 +7,6 @@ const commands = [
 
 var scr = document.getElementById("screen")
 var speed = 70;
-var login = 'github.io login: ';
 //var ps1 = 'amir@github.io:~$ ';
 var ps1 = '$ ';
 
@@ -39,7 +37,7 @@ async function user(t) {
 }
 
 
-scr.innerHTML += "<br />" + login;
+scr.innerHTML += "<br />" + ps1;
 var n = 0;
 function shell() {
 	if (n < commands.length) {
