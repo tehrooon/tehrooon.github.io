6f05ebfc03327d933cec44c51cdf1c51c720d322
amir <amirghassempour@protonmail.com>
2022-12-17 23:58:41 +0000

    divide shell.js and make commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,5 +1,6 @@
 const commands = [
 	["whoami", "amir"],
-	["echo $mail_proton", "4mi2@proton.me"],
-	["echo $mail_tutanota", "theamir@tutanota.com"]
+	["gpg -k", 'my pgp public key:<br />rsa4096 <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/C1826531F0F312291A461373479EC0B4C6FD024B">C1826531F0F312291A461373479EC0B4C5FD024B</a>'],
+	["echo $mailAddresses", "theamir@tutanota.com<br />4mi2@proton.me<br />please send mail from the same mail service."],
+	["echo $blockchainAddresses", "ETH: <br />SOL: "]
 ];
--- js/shell.js
+++ js/shell.js
@@ -1,10 +1,3 @@
-const commands = [
-	["whoami", "amir"],
-	["gpg -k", 'my pgp public key:<br />rsa4096 <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/C1826531F0F312291A461373479EC0B4C6FD024B">C1826531F0F312291A461373479EC0B4C5FD024B</a>'],
-	["echo $mailAddresses", "theamir@tutanota.com<br />4mi2@proton.me<br />please send mail from the same mail service."],
-	["echo $blockchainAddresses", "ETH: <br />SOL: "]
-];
-
 var scr = document.getElementById("screen")
 var speed = 70;
 //var ps1 = 'amir@github.io:~$ ';
