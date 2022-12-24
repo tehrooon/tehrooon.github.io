0c9921701f093c0ecc52efb75b688c7620838ec9
amir <amirghassempour@protonmail.com>
2022-12-24 12:20:08 +0000

    update commands.js -- login ascii art complete

--- js/commands.js
+++ js/commands.js
@@ -1,13 +1,21 @@
 const commands = [
-	["amir", "logged in as amir."],
+	["amir", `<pre>                       d8b         
+                       <span class="red">qop</span>        
+ 8888b.  88888b.d88b.  888 888d888 
+    "88b 888 "888 "88b 888 888P"   
+.d888888 888  888  888 888 888     
+888  888 888  888  888 888 888     
+"Y888888 888  888  888 888 888     <span class="red">2017-2022</span>
+_______________________Y8Y___________________
+<span class="red">________________________Y</span>_____________________</pre>`],
 	["whoami", "amir<br />self-educator & tech enthusiast."],
-	["more", "experiencer;<br />doc reader;<br />learner;<br />coder;<br />network & system admin."],
-	["echo $mailAddresses", "theamir@tutanota.com<br />4mi2@proton.me<br />0x411112@skiff.com<br />please send mail from the same mail service."],
-	["echo $blockchainAddresses", "ETH: 0xD893c1B6FB3a3D38ebe91cb3e463Cb1E4DcC2999<br />SOL: Fe6jVK6EzojEc1ymeYMziQJgkwy5M9mii7PJHUvNE4JD "],
-	["gpg -k", 'my pgp public key:<br />rsa4096 <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/C1826531F0F312291A461373479EC0B4C6FD024B">C1826531F0F312291A461373479EC0B4C5FD024B</a>'],
+	["more aboutMe", "experiencer;<br />doc reader;<br />learner;<br />coder;<br />network & system admin."],
+	["addresses mail --all", "theamir@tutanota.com<br />4mi2@proton.me<br />0x411112@skiff.com<br />please send mail from the same mail service."],
+	["addresses blockchain --all", "ETH: 0xD893c1B6FB3a3D38ebe91cb3e463Cb1E4DcC2999<br />SOL: Fe6jVK6EzojEc1ymeYMziQJgkwy5M9mii7PJHUvNE4JD "],
+	["pgp --list-public-key", 'my pgp public key:<br />rsa4096 <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/C1826531F0F312291A461373479EC0B4C6FD024B">C1826531F0F312291A461373479EC0B4C5FD024B</a>'],
 	["contact", '<a href="https://411112.github.io/contact">411112.github.io/contact</a>'],
-	['languages', 'english, deutsch'],
+	['languages --all', 'persian, english, deutsch, ...'],
 	['programming languages', 'bash, python, js, ...'],
 	['developing tools', 'git, docker'],
-	['policy', 'no binary file.<br />zero trust.']
+	['policy list ruleset', 'no binary file.<br />zero trust.']
 ];
